bc5e89bf7b0c221c973fbb16992d4f1a1fb5120a
David Marín <ddmarin94@gmail.com>
2019-01-12 11:08:08 +0100

    fix executable globally

--- bin/generate.js
+++ bin/generate.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
+
 const program = require('commander');
-const inquirer = require('inquirer');
 const fs = require('fs')
 const {stateless} = require('../template/stateless');
 const {statefull} = require('../template/statefull');
